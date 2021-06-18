6835ce29de949ce5e31daac1fd261f1ceeae9c22
EdgardoP <edgardoportillo05@gmail.com>
2021-06-18 15:31:49 -0600

    Actualizacion 14

--- edgardo/cheleeee.js
+++ edgardo/cheleeee.js
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
 </head>
+
 <body>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis vero minus voluptas deserunt! Possimus consequatur animi laudantium impedit cupiditate quasi culpa amet, reprehenderit ad voluptates eveniet inventore, blanditiis, ex non?</p>
-   <div>a</div>
-   <div>b</div>
-   <div>c</div>
-   <div>d</div>
-   <div></div>
-   <div></div>
-   <div></div>
-   <div></div>
-   <div></div>
-   <div></div>
-   <div></div>
-   <div></div>
-   <div></div>
-   <div></div>
+    <div>a</div>
+    <div>b</div>
+    <div>c</div>
+    <div>d</div>
+    <div>Jelouuuuuuuuuuuuuuuuuuu</div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
 </body>
+
 </html>
